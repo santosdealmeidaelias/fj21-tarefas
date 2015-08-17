cdc0e9bdc3efa58bdbdf7e666689c79e5dbb325b
Elias Santos de Almeida <santosdealmeidaelias@gmail.com>
2015-08-17 00:28:51 -0300

    Funcionalidade de adicionar tarefas implementado, e validação realizada com beans validation

---
+++ sql/create-database.sql
@@ -0,0 +1,10 @@
+create database fj21;
+use fj21;
+
+create table tarefas (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  descricao VARCHAR(255),
+  finalizado BOOLEAN,
+  dataFinalizacao DATE,
+  primary key (id)
+);
