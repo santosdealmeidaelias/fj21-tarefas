50a1986264ecafa22ec5e5ae3f87b703195514be
Elias Santos de Almeida <santosdealmeidaelias@gmail.com>
2015-08-17 01:11:35 -0300

    Funcionalidade: Finalizar Tarefa foi adicionada, Utilizando AJAX

--- src/br/com/caelum/tarefas/modelo/Tarefa.java
+++ src/br/com/caelum/tarefas/modelo/Tarefa.java
@@ -5,7 +5,7 @@ import java.util.Calendar;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
-
+import org.springframework.format.annotation.DateTimeFormat;
 
 public class Tarefa {
 	private Long id;
@@ -13,6 +13,8 @@ public class Tarefa {
 	@NotNull @Size(min=5)
 	private String descricao;
 	private boolean finalizado;
+	
+	@DateTimeFormat(pattern="dd/MM/yyyy")
 	private Calendar dataFinalizacao;
 
 	public Long getId() {
